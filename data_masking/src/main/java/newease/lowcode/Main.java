2af3b28fd014b092125e8565dff84c3790a29bfa
superName-w <wangzixuan183@163.com>
2024-03-06 22:24:30 +0800

    add：新增数据脱敏功能

---
+++ data_masking/src/main/java/newease/lowcode/Main.java
@@ -0,0 +1,12 @@
+package newease.lowcode;
+
+import newease.lowcode.data.masking.BulkDataMasking;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
