93d956cae2e78640bcb65a74e6771c4c0af12893
wangyongliang <viporg@126.com>
2024-03-18 23:05:43 +0800

    validate mobile phone with country code

--- RegexValidateUtil_fdddf/src/main/java/com/fdddf/regexvalidateutil/Main.java
+++ RegexValidateUtil_fdddf/src/main/java/com/fdddf/regexvalidateutil/Main.java
@@ -7,8 +7,9 @@ public class Main {
         System.out.println(RegexValidateUtilApi.isIdCard("410203198906283309"));
         System.out.println(RegexValidateUtilApi.isEmail("abc@163.com"));
         System.out.println(RegexValidateUtilApi.isMobile("13521456789"));
-        System.out.println(RegexValidateUtilApi.isPhone("0917-3385212"));
-        System.out.println(RegexValidateUtilApi.isPhone("3385212"));
+        System.out.println(RegexValidateUtilApi.isPhone("0917-3385212", true));
+        System.out.println(RegexValidateUtilApi.isPhone("3385212", false));
+        System.out.println(RegexValidateUtilApi.isMobilePhone("+86-16691850000", "CN"));
         System.out.println(RegexValidateUtilApi.IsUrl("https://www.baidu.com"));
         System.out.println(RegexValidateUtilApi.IsChinese("你好"));
         System.out.println(RegexValidateUtilApi.isIPv4("192.168.1.1"));
--- RegexValidateUtil_fdddf/src/main/java/com/fdddf/regexvalidateutil/api/RegexValidateUtilApi.java
+++ RegexValidateUtil_fdddf/src/main/java/com/fdddf/regexvalidateutil/api/RegexValidateUtilApi.java
@@ -1,13 +1,15 @@
 package com.fdddf.regexvalidateutil.api;
 
 import com.netease.lowcode.core.annotation.NaslLogic;
-import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Component
 public class RegexValidateUtilApi {
+
+    private static final Logger logger = LoggerFactory.getLogger(RegexValidateUtilApi.class);
 
     /**
      * 检查提供的字符串是否是有效的电子邮件地址。
@@ -22,7 +24,7 @@ public class RegexValidateUtilApi {
     }
 
     /**
-     * 判断传入的字符串是否为有效的手机号码。
+     * 判断传入的字符串是否为有效的国内手机号码。
      *
      * @param mobile 需要验证的手机号码字符串。
      * @return 返回布尔值，如果手机号码有效则返回true，否则返回false。
@@ -31,6 +33,102 @@ public class RegexValidateUtilApi {
     public static Boolean isMobile(String mobile) {
         // 使用正则表达式匹配手机号码格式
         return mobile.matches("^1[3456789]\\d{9}$");
+    }
+
+    /**
+     * 部分国家手机号码校验正则
+     */
+    public enum MobileRegularExp {
+        CN("中国", "^(\\+?0?86\\-?)?1[3456789]\\d{9}$"),
+        TW("台湾", "^(\\+?886\\-?|0)?9\\d{8}$"),
+        HK("香港", "^(\\+?852\\-?)?[569]\\d{3}\\-?\\d{4}$"),
+        MS("马来西亚", "^(\\+?6?01){1}(([145]{1}(\\-|\\s)?\\d{7,8})|([236789]{1}(\\s|\\-)?\\d{7}))$"),
+        PH("菲律宾", "^(\\+?0?63\\-?)?\\d{10}$"),
+        TH("泰国", "^(\\+?0?66\\-?)?\\d{10}$"),
+        SG("新加坡", "^(\\+?0?65\\-?)?\\d{10}$"),
+        /*以下是其他国家的手机号校验正则*/
+        DZ("阿尔及利亚", "^(\\+?213|0)(5|6|7)\\d{8}$"),
+        SY("叙利亚", "^(!?(\\+?963)|0)?9\\d{8}$"),
+        SA("沙特阿拉伯", "^(!?(\\+?966)|0)?5\\d{8}$"),
+        US("美国", "^(\\+?1)?[2-9]\\d{2}[2-9](?!11)\\d{6}$"),
+        CZ("捷克共和国", "^(\\+?420)? ?[1-9][0-9]{2} ?[0-9]{3} ?[0-9]{3}$"),
+        DE("德国", "^(\\+?49[ \\.\\-])?([\\(]{1}[0-9]{1,6}[\\)])?([0-9 \\.\\-\\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$"),
+        DK("丹麦", "^(\\+?45)?(\\d{8})$"),
+        GR("希腊", "^(\\+?30)?(69\\d{8})$"),
+        AU("澳大利亚", "^(\\+?61|0)4\\d{8}$"),
+        GB("英国", "^(\\+?44|0)7\\d{9}$"),
+        CA("加拿大", "^(\\+?1)?[2-9]\\d{2}[2-9](?!11)\\d{6}$"),
+        IN("印度", "^(\\+?91|0)?[789]\\d{9}$"),
+        NZ("新西兰", "^(\\+?64|0)2\\d{7,9}$"),
+        ZA("南非", "^(\\+?27|0)\\d{9}$"),
+        ZM("赞比亚", "^(\\+?26)?09[567]\\d{7}$"),
+        ES("西班牙", "^(\\+?34)?(6\\d{1}|7[1234])\\d{7}$"),
+        FI("芬兰", "^(\\+?358|0)\\s?(4(0|1|2|4|5)?|50)\\s?(\\d\\s?){4,8}\\d$"),
+        FR("法国", "^(\\+?33|0)[67]\\d{8}$"),
+        IL("以色列", "^(\\+972|0)([23489]|5[0248]|77)[1-9]\\d{6}"),
+        HU("匈牙利", "^(\\+?36)(20|30|70)\\d{7}$"),
+        IT("意大利", "^(\\+?39)?\\s?3\\d{2} ?\\d{6,7}$"),
+        JP("日本", "^(\\+?81|0)\\d{1,4}[ \\-]?\\d{1,4}[ \\-]?\\d{4}$"),
+        NO("挪威", "^(\\+?47)?[49]\\d{7}$"),
+        BE("比利时", "^(\\+?32|0)4?\\d{8}$"),
+        PL("波兰", "^(\\+?48)? ?[5-8]\\d ?\\d{3} ?\\d{2} ?\\d{2}$"),
+        BR("巴西", "^(\\+?55|0)\\-?[1-9]{2}\\-?[2-9]{1}\\d{3,4}\\-?\\d{4}$"),
+        PT("葡萄牙", "^(\\+?351)?9[1236]\\d{7}$"),
+        RU("俄罗斯", "^(\\+?7|8)?9\\d{9}$"),
+        RS("塞尔维亚", "^(\\+3816|06)[- \\d]{5,9}$"),
+        TR("土耳其", "^(\\+?90|0)?5\\d{9}$"),
+        VN("越南", "^(\\+?84|0)?((1(2([0-9])|6([2-9])|88|99))|(9((?!5)[0-9])))([0-9]{7})$"),
+        /* 以下是匹配所有手机号校验正则*/
+        OT("所有", "^(\\+?0)?([0-9]*[1-9][0-9]*)$");
+
+        /**
+         * 国际名称
+         */
+        private final String national;
+
+        /**
+         * 正则表达式
+         */
+        private final String regularExp;
+
+        MobileRegularExp(String national, String regularExp) {
+            this.national = national;
+            this.regularExp = regularExp;
+        }
+
+        public String getNational() {
+            return national;
+        }
+
+        public String getRegularExp() {
+            return regularExp;
+        }
+    }
+
+    /**
+     * 检查一个字符串是否为有效的中国手机号码。
+     *
+     * @param mobile 待检查的手机号码字符串。
+     * @param countryCode 国家代码，目前支持的国家代码有：CN,TW,HK,MS,PH,TH,SG,DZ,SY,
+     *                    SA,US,CZ,DE,DK,GR,AU,GB,CA,IN,NZ,ZA,ZM,ES,FI,FR,IL,HU,
+     *                    IT,JP,NO,BE,PL,BR,PT,RU,RS,TR,VN
+     *                    全部国家为OT
+     * @return Boolean
+     */
+    public static Boolean isMobilePhone(String mobile, String countryCode) {
+        boolean isMobileNumber = false;
+        try {
+            MobileRegularExp regularExp = MobileRegularExp.valueOf(countryCode);
+            isMobileNumber = mobile.matches(regularExp.getRegularExp());
+        } catch (IllegalArgumentException e) {
+            if (countryCode.equals("OT")) {
+                MobileRegularExp regularExp = MobileRegularExp.OT;
+                isMobileNumber = mobile.matches(regularExp.getRegularExp());
+            } else {
+                logger.error("不支持的国家代码：" + countryCode);
+            }
+        }
+        return isMobileNumber;
     }
 
     /**
@@ -46,16 +144,14 @@ public class RegexValidateUtilApi {
     }
 
     /**
-     * 检查提供的字符串是否为电话号码。
-     * 该函数支持两种格式的电话号码：带区号和不带区号。
-     * 带区号的电话号码格式为：0xx-xxxxxxxx（x为数字，区号为2-3位，电话号码为5-10位）。
-     * 不带区号的电话号码格式为：xxxxxxxxxx（x为数字，电话号码为6-9位）。
+     * 检查提供的字符串是否为电话号码
+     * 该函数支持两种格式的电话号码：带区号和不带区号
      *
      * @param str 需要检查的字符串。
-     * @return 如果提供的字符串符合上述任一电话号码格式，则返回true；否则返回false。
+     * @return Boolean
      */
     @NaslLogic
-    public static Boolean isPhone(final String str) {
+    public static Boolean isPhone(final String str, Boolean withAreaCode) {
         Pattern p1, p2;  // 定义两个正则表达式模式，分别用于匹配带区号和不带区号的电话号码
         Matcher m;       // 匹配器，用于匹配字符串和正则表达式
         boolean b;       // 用于存储匹配结果的布尔值
@@ -65,15 +161,14 @@ public class RegexValidateUtilApi {
         // 匹配不带区号的电话号码的正则表达式
         p2 = Pattern.compile("^[1-9]{1}[0-9]{5,8}$");
 
-        if (str.length() > 9) {
+        if (str.length() > 9 && withAreaCode) {
             // 如果字符串长度大于9，认为它可能是带区号的电话号码
             m = p1.matcher(str);
-            b = m.matches();
         } else {
             // 否则，认为它可能是不带区号的电话号码
             m = p2.matcher(str);
-            b = m.matches();
         }
+        b = m.matches();
 
         return b; // 返回匹配结果
     }
@@ -111,6 +206,6 @@ public class RegexValidateUtilApi {
     @NaslLogic
     public static Boolean IsChinese(String str) {
         // 使用正则表达式匹配字符串，判断是否只包含中文字符
-        return str.matches("^[\u4e00-\u9fa5]+$");
+        return str.matches("^[\\u4e00-\\u9fa5]+$");
     }
 }
