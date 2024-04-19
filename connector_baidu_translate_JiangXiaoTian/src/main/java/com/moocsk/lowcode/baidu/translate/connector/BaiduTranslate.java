369a88d014b2e26151420773025aec277e00697c
moocsk <moocsk@163.com>
2024-04-19 18:20:19 +0800

    百度翻译连接器 添加翻译内容长度限制

--- connector_baidu_translate_JiangXiaoTian/src/main/java/com/moocsk/lowcode/baidu/translate/connector/BaiduTranslate.java
+++ connector_baidu_translate_JiangXiaoTian/src/main/java/com/moocsk/lowcode/baidu/translate/connector/BaiduTranslate.java
@@ -7,6 +7,7 @@ import com.moocsk.lowcode.baidu.translate.api.TransApi;
 import com.moocsk.lowcode.baidu.translate.model.TranslateResult;
 import com.moocsk.lowcode.baidu.translate.model.TranslateResultSingle;
 import com.moocsk.lowcode.baidu.translate.util.ModelUtil;
+import com.moocsk.lowcode.baidu.translate.util.StringUtil;
 import com.netease.lowcode.core.annotation.NaslConnector;
 
 /**
@@ -23,7 +24,8 @@ public class BaiduTranslate {
 
     /**
      * 初始化
-     * @param appid 应用ID
+     * 
+     * @param appid     应用ID
      * @param secretKey 密钥
      * @return 百度翻译连接器
      */
@@ -37,7 +39,8 @@ public class BaiduTranslate {
 
     /**
      * 连通性测试
-     * @param appid 应用ID
+     * 
+     * @param appid     应用ID
      * @param secretKey 密钥
      * @return 连通结果
      */
@@ -53,31 +56,50 @@ public class BaiduTranslate {
 
     /**
      * 单条文本翻译
-     * @param q 需要翻译的内容
+     * 
+     * @param q    需要翻译的内容
      * @param from 源语言
-     * @param to 目标语言
+     * @param to   目标语言
      * @return 单条翻译结果
      */
     @NaslConnector.Logic
     public TranslateResultSingle translation(String q, String from, String to) {
+        TranslateResultSingle translateResultSingle = new TranslateResultSingle();
+        // 翻译文本总长度控制在 6000 字节以内
+        int qLen = StringUtil.getStringLengthByByte(q); // 翻译文本字节长度
+        if (qLen > 6000) {
+            translateResultSingle.setErrorCode("500");
+            translateResultSingle.setErrorMsg("请将翻译内容控制在6000字节以内");
+            return translateResultSingle;
+        }
         TransApi transApi = new TransApi(this.appid, this.secretKey);
         TranslateResult translateResult = transApi.generalTextTranslation(q, from, to);
-        TranslateResultSingle translateResultSingle = ModelUtil.getSingleTranslate(translateResult);
+        translateResultSingle = ModelUtil.getSingleTranslate(translateResult);
         return translateResultSingle;
     }
 
     /**
      * 批量文本翻译
-     * @param q 需要翻译的内容
+     * 
+     * @param q    需要翻译的内容
      * @param from 源语言
-     * @param to 目标语言
+     * @param to   目标语言
      * @return 批量翻译结果
      */
     @NaslConnector.Logic
     public TranslateResult translationBatch(List<String> q, String from, String to) {
+        TranslateResult translateResult = new TranslateResult();
         String qStr = String.join("\n", q);
+        // 批量翻译文本总长度控制在 6000 字节以内
+        int qStrLen = StringUtil.getStringLengthByByte(qStr); // 翻译文本字节长度
+        if (qStrLen > 6000) {
+            translateResult.setErrorCode("500");
+            translateResult.setErrorMsg("请将翻译内容控制在6000字节以内");
+            return translateResult;
+        }
         TransApi transApi = new TransApi(this.appid, this.secretKey);
-        return transApi.generalTextTranslation(qStr, from, to);
+        translateResult = transApi.generalTextTranslation(qStr, from, to);
+        return translateResult;
     }
 
     public static void main(String[] args) {
@@ -91,25 +113,26 @@ public class BaiduTranslate {
         // 注意：以下测试片段不能同时调用
 
         // 测试连通性
-        Boolean connBoolean = baiduTranslate.testConnection(appid, secretKey);
-        if (connBoolean) {
-            System.out.println("连接成功");
-        } else {
-            System.out.println("连接失败");
-        }
+        // Boolean connBoolean = baiduTranslate.testConnection(appid, secretKey);
+        // if (connBoolean) {
+        // System.out.println("连接成功");
+        // } else {
+        // System.out.println("连接失败");
+        // }
 
         // 测试批量文本翻译
-        // List<String> list = new ArrayList<String>();
-        // list.add("水果");
-        // list.add("香蕉");
-        // TranslateResult translateResult = baiduTranslate.translationBatch(list, from, to);
-        // System.out.println("批量文本翻译结果");
-        // System.out.println(translateResult);
+        List<String> list = new ArrayList<String>();
+        list.add("水果");
+        list.add("香蕉");
+        TranslateResult translateResult = baiduTranslate.translationBatch(list, from, to);
+        System.out.println("批量文本翻译结果");
+        System.out.println(translateResult);
 
         // 测试单条文本翻译
-        // TranslateResultSingle translateResultSingle = baiduTranslate.translation("水果", from, to);
+        // TranslateResultSingle translateResultSingle =
+        // baiduTranslate.translation("水果", from, to);
         // System.out.println("单条文本翻译结果");
         // System.out.println(translateResultSingle);
-     }
-    
+    }
+
 }
