dabe1348e6d3a141adcf2da9abfa0044a1aae84e
wybieshang <1175929873@qq.com>
2024-06-04 14:25:00 +0800

    feat: lottie-upgrade

---
+++ cw_lottie_library/vusion.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'mouse',path: "./components/mouse/api.yaml"},,
+            { group: '组件', name: 'animated-composition',path: "./components/animated-composition/api.yaml"},,
+            { group: '组件', name: 'sprite-animation',path: "./components/sprite-animation/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+};
