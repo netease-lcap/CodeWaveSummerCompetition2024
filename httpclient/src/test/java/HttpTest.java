d26d25a8dca1553e14d56b02f55e04b07bb7a478
baojz <baojz@guahao.com>
2024-06-12 19:59:19 +0800

    新增异常处理

--- httpclient/src/test/java/HttpTest.java
+++ httpclient/src/test/java/HttpTest.java
@@ -1,7 +1,6 @@
 import com.netease.http.config.NosConfig;
 import com.netease.http.dto.RequestParam;
 import com.netease.http.dto.RequestParamAllBodyType;
-import com.netease.http.dto.RequestParamAllBodyTypeInner;
 import com.netease.http.httpclient.HttpClientService;
 import com.netease.http.httpclient.LCAPHttpClient;
 import com.netease.http.spring.HttpSpringEnvironmentConfiguration;
@@ -16,7 +15,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Resource;
-import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Date;
@@ -25,11 +23,9 @@ import java.util.Map;
 import java.util.Objects;
 
 
-//@SpringBootTest(classes = HttpSpringEnvironmentConfiguration.class)
-//@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HttpSpringEnvironmentConfiguration.class)
+@RunWith(SpringRunner.class)
 public class HttpTest {
-    public static final String APP_ID = "ZQ_PMS";
-    public static final String APP_SECRET = "bc25e4c6428d47ec94a051a5614cecc1";
     @Resource
     private RestTemplate restTemplate;
     @Resource
@@ -37,50 +33,43 @@ public class HttpTest {
     @Resource
     private HttpClientService httpClientService;
 
-//    @Test
+    @Test
     public void testV2() {
-        String url = "https://id-test.163yun.com/v3/account/codeWave/login";
+        String url = "";
         Map<String, String> head = new HashMap<>();
         head.put("Content-Type", "application/json");
         head.put("User-Agent", "Apifox/1.0.0 (https://apifox.com)");
         Map<String, String> body = new HashMap<>();
-        body.put("userName", "17340526870");
-        body.put("password", "89dea581d0d3dc1030a15fe6cc615d29");
+        body.put("userName", "222");
+        body.put("password", "11");
         String urlRes = lcapHttpClient.exchangeV2(url, HttpMethod.POST.name(), head, body);
         System.out.println(urlRes);
     }
 
-//    @Test
+    @Test
     public void testHttp() {
-        String fileUrl = "https://dev.bjztest.neteasepmo.lcap.163yun.com/upload/app/TileMapUtils_%E4%BE%9D%E8%B5%96%E5%BA%93%E4%BD%BF%E7%94%A8%E6%96%87%E6%A1%A3%E8%AF%B4%E6%98%8E_20240521100623466.docx";
+        String fileUrl = "";
 
         RequestParamAllBodyType requestParam = new RequestParamAllBodyType();
         requestParam.setUrl(fileUrl);
         requestParam.setHttpMethod("get");
-//        Map<String, String> head = new HashMap<>();
-//        head.put("Authorization", "Basic MDJhZGUxZDZmZWQyNGIzY2JiNGQ5Yjg5MzUxY2RlNTY6NWRlOTliMzkwMmQzNGIzNzhmNDI2OTIxZmVhMjNjYzA=");
-//        requestParam.setHeader(head);
-//        String body = "param1=value1&param2=value2";
-//        requestParam.setBody(body);
         requestParam.setIsIgnoreCrt(false);
         String url = lcapHttpClient.exchangeAllBodyType(requestParam);
         System.out.println(url);
         requestParam.setIsIgnoreCrt(true);
-//        url = lcapHttpClient.exchangeAllBodyType(requestParam);
-//        System.out.println(url);
     }
 
-//    @Test
+    //    @Test
     public void testHttpBody() throws URISyntaxException {
         RequestParam requestParam = new RequestParam();
-        requestParam.setUrl("http://wowwwrk.netease.com/itsm/v1/system/login/local");
+        requestParam.setUrl("");
         requestParam.setHttpMethod("POST");
         Map<String, String> head = new HashMap<>();
 //        head.put("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
         requestParam.setHeader(head);
         Map<String, String> body = new HashMap<>();
-        body.put("username", "airconditioner");
-        body.put("password", "U2FsdGVkX19pvpydcUlekP9nbbI8wGcj9fbvVjvenSo=");
+        body.put("username", "2131");
+        body.put("password", "21312123=");
         requestParam.setBody(body);
 //        requestParam.setIsIgnoreCrt(true);
         try {
@@ -94,19 +83,19 @@ public class HttpTest {
 //        System.out.println(url);
     }
 
-//    @Test
+    //    @Test
     public void testDownload() {
         URI uri = UriComponentsBuilder
-                .fromUriString("https://crpdev.fin.netease.com/api/openapi/contract/attachment/download")
-                .queryParam("uid", "10000566")
+                .fromUriString(" ")
+                .queryParam("uid", "3213131")
                 .encode()
                 .build()
                 .toUri();
         HttpHeaders authHeader = OpenApiTokens.builder()
                 .uri(uri)
                 .body(null)
-                .appId(APP_ID)
-                .signWith(APP_SECRET)
+                .appId("APP_ID")
+                .signWith("APP_SECRET")
                 .issuedAt(new Date())
                 .compact();
         Map<String, String> header = new HashMap<>();
@@ -114,55 +103,12 @@ public class HttpTest {
         header.put("timestamp", Objects.requireNonNull(authHeader.get(OpenApiTokens.AUTH_HEADER_TIMESTAMP)).get(0));
         header.put("signature", Objects.requireNonNull(authHeader.get(OpenApiTokens.AUTH_HEADER_SIGNATURE)).get(0));
         NosUtil.nosConfig = new NosConfig();
-        NosUtil.nosConfig.nosAccessKey = "e09bc35070b3406eb4179c332da67146";
-        NosUtil.nosConfig.nosSecretKey = "624e6295a44d44f7880ae968b10d0892";
-        NosUtil.nosConfig.nosAddress = "nos-eastchina1.126.net";
-        NosUtil.nosConfig.nosBucket = "lcpapp-static";
-        NosUtil.nosConfig.sinkType = "nos";
+        NosUtil.nosConfig.nosAccessKey = "11";
+        NosUtil.nosConfig.nosSecretKey = "11";
+        NosUtil.nosConfig.nosAddress = "11";
+        NosUtil.nosConfig.nosBucket = "11";
+        NosUtil.nosConfig.sinkType = "11";
         String url = lcapHttpClient.downloadFileUploadNos(uri.toString(), null, header);
         System.out.println(url);
-    }
-
-//    @Test
-    public void testDownloadAll() {
-        Map<String, String> header = new HashMap<>();
-        header.put("access-token", "a3dfd639eba440cfbfff58282012d225");
-        NosUtil.nosConfig = new NosConfig();
-        NosUtil.nosConfig.nosAccessKey = "e09bc35070b3406eb4179c332da67146";
-        NosUtil.nosConfig.nosSecretKey = "624e6295a44d44f7880ae968b10d0892";
-        NosUtil.nosConfig.nosAddress = "nos-eastchina1.126.net";
-        NosUtil.nosConfig.nosBucket = "lcpapp-static";
-        NosUtil.nosConfig.sinkType = "nos";
-        String url = lcapHttpClient.downloadFileUploadNos("http://61.164.40.52:40056/dev-dept-kpi-api/sysportal/ztTask/exportTaskPerson.action?data={\"openedby\":\"\",\"assigneddate\":\"\",\"finisheddate\":\"\",\"closedby\":\"\",\"closeddate\":\"\",\"name\":\"\",\"statusList\":\"\",\"assignedto\":\"\",\"finishedby\":\"\",\"pdtId\":\"\",\"project\":\"\"}",
-                null, header);
-        System.out.println(url);
-    }
-
-    //    @Test
-    public void testDownload2() {
-        RequestParamAllBodyTypeInner requestParam = new RequestParamAllBodyTypeInner();
-        requestParam.setUrl("https://dev-test-baohuahua.app.codewave.163.com/upload/app/1704163660141_20240105111142008.jpg");
-        requestParam.setHttpMethod(HttpMethod.GET.name());
-        File file = httpClientService.downloadFile(requestParam, restTemplate, null);
-        File file1 = new File("a" + file.getName());
-        file.renameTo(file1);
-        System.out.println(file);
-    }
-
-    //    @Test
-    public void testDownloadFileUploadNos() {
-        RequestParam requestParam = new RequestParam();
-        Map<String, String> head = new HashMap<>();
-        requestParam.setHeader(head);
-        Map<String, String> body = new HashMap<>();
-        requestParam.setBody(body);
-        requestParam.setHttpMethod("post");
-
-        body.put("formatCheck", "false");
-        head.put("X-Token", "ubF9PgJPWExFY5HG0SzMjjNwvy63bazgqv6mhXo8UvNo6aPYazjRGs5scQk-C2X3");
-        requestParam.setUrl("https://uat.cdp.changan.com.cn/trading-desk/api/brief/upload/new");
-        String res = lcapHttpClient.uploadNosExchange("/upload/app/中文测试_20240110144435430.jpg",
-                "https://dev-test-baohuahua.app.codewave.163.com/test_upload", requestParam);
-        System.out.println(res);
     }
 }
