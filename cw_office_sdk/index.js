cfdbaa3a3e9a6983f5d6d1be2d29763023ef8dd8
gungunj <741127583@qq.com>
2024-06-28 14:10:14 +0800

    feat:office依赖库开源

---
+++ cw_office_sdk/index.js
@@ -0,0 +1,16 @@
+import CwPdfPreview from './components/cw-pdf-preview';
+import CwDocPreview from './components/cw-doc-preview';
+import CwPdfEditor from './components/cw-pdf-editor';
+import CwXlsxPreview from './components/cw-xlsx-preview';
+import CwPptPreview from './components/cw-ppt-preview';
+import UtilsLogics from './logics';
+// COMPONENT IMPORTS
+export {
+	CwPdfPreview,
+	CwDocPreview,
+	CwPdfEditor,
+	CwXlsxPreview,
+	CwPptPreview,
+	UtilsLogics,
+// COMPONENT EXPORTS
+};
---
+++ cw_office_sdk/vusion.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    type: 'component',
+    docs: {
+        components: [
+            { group: '组件', name: 'cw-pdf-preview',path: "./components/cw-pdf-preview/api.yaml"},
+            { group: '组件', name: 'cw-doc-preview',path: "./components/cw-doc-preview/api.yaml"},
+            { group: '组件', name: 'cw-pdf-editor',path: "./components/cw-pdf-editor/api.yaml"},
+            { group: '组件', name: 'cw-xlsx-preview',path: "./components/cw-xlsx-preview/api.yaml"},,
+            { group: '组件', name: 'cw-ppt-preview',path: "./components/cw-ppt-preview/api.yaml"},,
+            // Conponents Route List
+        ],
+    },
+    docStaticURL:"https://static-vusion.nos.lcap.163yun.com"
+};
