39677e2ee9f54352f2817a7f039f313dae27953c
kehfan <kehfan@yeah.net>
2024-07-10 15:57:59 +0800

    增加熔断配置

--- OpenFeign/src/main/java/com/netease/lowcode/extension/feign/ExtensionUserClientWithFallback.java
+++ OpenFeign/src/main/java/com/netease/lowcode/extension/feign/ExtensionUserClientWithFallback.java
@@ -1,13 +1,11 @@
 package com.netease.lowcode.extension.feign;
 
-import com.netease.lowcode.extension.OpenFeignCustomConfig;
 import com.netease.lowcode.extension.service.ExtensionUserClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "extensionUserClientWithFallback",
         url = "http://dev.nacos.cstest.lcap.codewave-test.163yun.com",
-        configuration = OpenFeignCustomConfig.class,
         fallback = ExtensionUserClientFallback.class)
 public interface ExtensionUserClientWithFallback {
 
