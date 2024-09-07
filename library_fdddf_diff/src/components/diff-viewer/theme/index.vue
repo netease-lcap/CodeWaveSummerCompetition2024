1065af109664fbb8a2f7df178dfd133fabd6dc74
wangyongliang <viporg@126.com>
2024-09-08 02:06:08 +0800

    #feat text compare first commit

---
+++ library_fdddf_diff/src/components/diff-viewer/theme/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <diff-viewer />
+</template>
+<script>
+import createStoriesPreview from '@lcap/builder/input/vue2/stories-preview';
+import * as stories from '../stories/block.stories';
+
+const DiffPreview = createStoriesPreview(stories);
+
+export default {
+components: {
+    DiffPreview,
+},
+};
+
+</script>
---
+++ library_fdddf_diff/src/logics/index.ts
@@ -0,0 +1,81 @@
+import '@nasl/types';
+
+import { diffLines, diffChars, diffWords } from 'diff';
+
+export {};
+
+
+/**
+ * @NaslLogic
+ * @type both
+ * @title 行差异
+ * @desc 行差异
+ * @param one 第一个文本
+ * @param other 第二个文本
+ * @returns 返回结果 { value: string, added: bool, removed: bool, count: number }
+ */
+export function getDiffLines(one: nasl.core.String, other: nasl.core.String): nasl.collection.List<nasl.collection.Map<nasl.core.String, nasl.core.String>> {
+    if (one === other) {
+        return [];
+    }
+    const result = diffLines(one, other);
+    return convertChangedObjects(result);
+}
+
+
+/**
+ * @NaslLogic
+ * @type both
+ * @title 字符差异
+ * @desc 字符差异
+ * @param one 第一个文本
+ * @param other 第二个文本
+ * @returns 返回结果 { value: string, added: bool, removed: bool, count: number }
+ */
+export function getDiffChars(one: nasl.core.String, other: nasl.core.String): nasl.collection.List<nasl.collection.Map<nasl.core.String, nasl.core.String>> {
+    if (one === other) {
+        return [];
+    }
+    const result = diffChars(one, other);
+    return convertChangedObjects(result);
+}
+
+
+/**
+ * @NaslLogic
+ * @type both
+ * @title 词差异
+ * @desc 词差异
+ * @param one 第一个文本
+ * @param other 第二个文本
+ * @returns 返回结果 { value: string, added: bool, removed: bool, count: number }
+ */
+export function getDiffWords(one: nasl.core.String, other: nasl.core.String): nasl.collection.List<nasl.collection.Map<nasl.core.String, nasl.core.String>> {
+    if (one === other) {
+        return [];
+    }
+    const result = diffWords(one, other);
+    return convertChangedObjects(result);
+}
+
+
+function convertChangedObjects(changedObjects: any[]) {
+    // Initialize the result as a list of DiffObject
+    const result: nasl.collection.List<nasl.collection.Map<nasl.core.String, nasl.core.String>> = [];
+  
+    // Iterate through each item in the changedObjects array
+    changedObjects.forEach(item => {
+      // Create a DiffObject with the required fields
+      const obj: nasl.collection.Map<nasl.core.String, nasl.core.String> = {
+        value: item.value,
+        added:  item.added ? "1" : "0",
+        removed: item.removed ? "1" : "0",
+        count: item.count + ""
+      };
+  
+      // Add the object to the result list
+      result.push(obj);
+    });
+  
+    return result;
+}
