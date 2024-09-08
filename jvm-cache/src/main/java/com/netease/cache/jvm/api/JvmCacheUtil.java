96f094a0e77c38c213cbd4fe199df1744f44320c
xujianping <xujianping@corp.netease.com>
2024-09-08 16:29:30 +0800

    新增jvm缓存

---
+++ jvm-cache/src/main/java/com/netease/cache/jvm/api/JvmCacheUtil.java
@@ -0,0 +1,117 @@
+package com.netease.cache.jvm.api;
+
+import com.netease.lowcode.core.EnvironmentType;
+import com.netease.lowcode.core.annotation.Environment;
+import com.netease.lowcode.core.annotation.NaslConfiguration;
+import com.netease.lowcode.core.annotation.NaslLogic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.function.Function;
+
+@Component
+public class JvmCacheUtil {
+
+    private static FlexibleExpirationCache<String, String> cache;
+
+    /**
+     * 缓存空间大小
+     * 类型为Long
+     */
+    @Value("${cacheMaximumSize}")
+    @NaslConfiguration(defaultValue = {@Environment(type = EnvironmentType.DEV, value = "100"),
+            @Environment(type = EnvironmentType.ONLINE, value = "100")})
+    public Long cacheMaximumSize;
+
+
+    @PostConstruct
+    public void init() {
+        cache = new FlexibleExpirationCache<>(cacheMaximumSize);
+    }
+
+    /**
+     * 添加缓存
+     *
+     * @param key 键
+     * @param value 值
+     * @param expireAfterWriteSeconds 过期时间（秒）
+     */
+    @NaslLogic
+    public String setCache(String key, String value, Long expireAfterWriteSeconds) {
+        cache.put(key, value, expireAfterWriteSeconds);
+        return value;
+    }
+
+    /**
+     * 获取缓存
+     *
+     * @param key 键
+     * @return 值
+     */
+    @NaslLogic
+    public String getCache(String key) {
+        return cache.get(key);
+    }
+
+    /**
+     * 获取缓存，缓存不存在则执行function， 并将查询的值放到缓存中
+     */
+    @NaslLogic
+    public String getIsNullElseFromDBAndSet(String key, Long expireAfterWriteSeconds, Function<String, String> functionStr) {
+        String value = getCache(key);
+        if (value != null) {
+            return value;
+        }
+
+        String str = functionStr.apply(null);
+        if (str != null) {
+            setCache(key, str, expireAfterWriteSeconds);
+        }
+        return str;
+    }
+
+    /**
+     * 是否过期
+     *
+     * @param key 键
+     * @return true/false
+     */
+    @NaslLogic
+    public Boolean isExpired(String key) {
+        return cache.isExpired(key);
+    }
+
+    /**
+     * 清除缓存
+     *
+     * @param key 键
+     */
+    @NaslLogic
+    public Boolean invalidate(String key) {
+        cache.invalidate(key);
+        return true;
+    }
+
+    /**
+     * 清除所有缓存
+     *
+     * @param key 键
+     */
+    @NaslLogic
+    public Boolean invalidateAll() {
+        cache.invalidateAll();
+        return true;
+    }
+
+    /**
+     * 查询缓存中所有的key
+     */
+    @NaslLogic
+    public List<String> keys() {
+        return cache.getAllKeys();
+    }
+
+
+}
