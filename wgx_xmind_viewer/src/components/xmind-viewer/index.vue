cf472f294059c17d27a478d5abc37f22dc46b939
superName-w <1804474775@qq.com>
2024-09-24 00:26:43 +0800

    feat：新增加载动画，修复IDE修改url导致在开发界面编辑状态下渲染图表，让他始终保持加载动画。

--- wgx_xmind_viewer/src/components/xmind-viewer/index.vue
+++ wgx_xmind_viewer/src/components/xmind-viewer/index.vue
@@ -1,75 +1,133 @@
 <template>
   <div id="xmind-container">
+    <div v-if="showLoading" class="loading"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "xmind-online-viewer",
   props: {
     fileUrl: {
       type: String,
-      default: null,
+      //default: 'http://localhost:8008/中心主题.xmind',
     }
   },
   data() {
     return {
+      showLoading: true,
       viewer: null
-    };
-  },
-  watch: {
-    // 监听 url 属性
-    fileUrl: {
-      // 确保在初始化时也执行一次
-      immediate: true, 
-      handler: function(newUrl) {
-        // 当 url 属性发生变化时，重新加载 xmind 文件
-        this.loadXMindFile(newUrl);
-      }
     }
   },
-  methods: {
-    initViewer() {
-      import('xmind-embed-viewer').then(XMindEmbedViewer => {
-        this.viewer = new XMindEmbedViewer.XMindEmbedViewer({
-          el: '#xmind-container',
-          region: 'cn'
-        });
-        this.viewer.setStyles({
-          width: '100%',
-          height: '100%'
-        });
-      }).catch(err => {
-        console.error('导入xmind-embed-viewer失败！', err);
-      });
-    },
-    async loadXMindFile(url) {
-      try {
-        const response = await fetch(url);
-        const arrayBuffer = await response.arrayBuffer();
-        this.viewer.load(arrayBuffer);
-      } catch (err) {
-        console.error('加载xmind文件出错：', err);
-      } 
-      // 由于不再显示加载动画，以下代码可以移除或注释掉
-      // this.showLoading = false; // 文件加载完成后隐藏加载动画
+  watch: {
+    // 监听 url 属性的变化
+    fileUrl: {
+      immediate: false,
+      handler: 'loadXMind'
     }
   },
   mounted() {
-    // 初始化 xmind 插件
-    this.initViewer();
-  }
+    this.loadXMind()
+  },
+  methods: {
+    async loadXMind() {
+      // 显示加载动画
+      this.showLoading = true;
+      // 判断是否在IDE中运行
+      if(process.env.VUE_APP_DESIGNER){
+          this.fileUrl = '';
+      }
+      if (this.viewer) {
+        // 如果实例已存在，仅加载新的文件
+        try {
+          const response = await fetch(this.fileUrl);
+          const file = await response.arrayBuffer();
+          this.viewer.load(file);
+        } catch (err) {
+          this.onError(err);
+        }
+      } else {
+        // 如果实例不存在，创建新实例并加载文件
+        try {
+          const { XMindEmbedViewer } = await import('xmind-embed-viewer');
+          this.viewer = new XMindEmbedViewer({
+            el: '#xmind-container',
+            region: 'cn'
+          });
+          this.viewer.setStyles({
+            width: '100%',
+            height: '100%'
+          });
+          this.viewer.addEventListener('map-ready', this.onMapReady);
+          this.viewer.addEventListener('error', this.onError);
+
+          const response = await fetch(this.fileUrl);
+          const file = await response.arrayBuffer();
+          this.viewer.load(file);
+        } catch (err) {
+          this.onError(err);
+        }
+      }
+    },
+    onMapReady() {
+      this.showLoading = false
+    },
+    onError(err) {
+      this.showLoading = false
+      console.error('加载xmind文件出错！', err)
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
 #xmind-container {
-  position: relative; /* 使容器成为一个相对定位的元素 */
-  width: 100%; /* 或者设置为特定宽度 */
-  height: 100%; /* 或者设置为特定高度 */
-  display: flex; /* 使用 flexbox 布局 */
-  justify-content: center; /* 水平居中 */
-  align-items: center; /* 垂直居中 */
+  display: flex;
+  height: 100%;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
 }
 
+.loading {
+  display: block;
+  position: absolute;
+  width: 6px;
+  height: 10px;
+  animation: rectangle infinite 1s ease-in-out -0.2s;
+  background-color: #000;
+}
+
+.loading:before,
+.loading:after {
+  position: absolute;
+  width: 6px;
+  height: 10px;
+  content: "";
+  background-color: #000;
+}
+
+.loading:before {
+  left: -14px;
+  animation: rectangle infinite 1s ease-in-out -0.4s;
+}
+
+.loading:after {
+  right: -14px;
+  animation: rectangle infinite 1s ease-in-out;
+}
+
+@keyframes rectangle {
+
+  0%,
+  80%,
+  100% {
+    height: 20px;
+    box-shadow: 0 0 #000;
+  }
+
+  40% {
+    height: 30px;
+    box-shadow: 0 -20px #000;
+  }
+}
 </style>
