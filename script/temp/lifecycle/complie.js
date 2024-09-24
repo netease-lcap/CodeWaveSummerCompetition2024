0d3575b424196059e8418a7f855fe142eca45637
Li Yingjun <lyjamare@163.com>
2024-09-24 16:13:51 +0800

    Update complie.js

--- script/temp/lifecycle/complie.js
+++ script/temp/lifecycle/complie.js
@@ -32,6 +32,8 @@ const feBuild = async (package) => {
     await fsp.access(tsconfigPath, fs.constants.F_OK);
     console.log('tsconfig.json 存在，不执行 "npm run usage" 命令。');
   } catch (err) {
+    console.log(tsconfigPath);
+    console.log(err);
     // 如果 tsconfig.json 不存在，则执行 "npm run usage"
     await execCommand('npm run usage', {
       throwWhenStderr: false,
