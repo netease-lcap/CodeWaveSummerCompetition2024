b44dfb2f8dd08b054415b532446672fcb49a34a4
baojz <baojz@guahao.com>
2024-09-24 16:23:19 +0800

    init

--- lcap-redis-connector/src/test/java/com/netease/cloud/RedisTest.java
+++ lcap-redis-connector/src/test/java/com/netease/cloud/RedisTest.java
@@ -9,12 +9,12 @@ public class RedisTest {
 
     private static void testConnection(){
         RedisConnector redisConnector = new RedisConnector();
-        System.out.println(redisConnector.testConnection("127.0.0.1", 39383,"d!fd2kgdD8",0));
+        System.out.println(redisConnector.testConnection("127.0.0.1", 39383,"",0));
     }
 
      private static void testGetSetValue(){
         RedisConnector redisConnector = new RedisConnector();
-        redisConnector.initRedisTemplate("127.0.0.1", 39383, "d!fd2kgdD8", 0);
+        redisConnector.initRedisTemplate("127.0.0.1", 39383, "", 0);
         redisConnector.setValue("jtsKey","jtsValue");
         String jtsKey = redisConnector.getValue("jtsKey");
         System.out.println(jtsKey);
