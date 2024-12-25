469ca26166ad916fc5b7825c7b2097e643c62077
baojz <baojz@guahao.com>
2024-12-25 20:17:53 +0800

    1.3.17版本去除mock压测

--- custon-api-filter/src/main/java/com/netease/lowcode/custonapifilter/sign/impl/NonceCheckServiceImpl.java
+++ custon-api-filter/src/main/java/com/netease/lowcode/custonapifilter/sign/impl/NonceCheckServiceImpl.java
@@ -46,33 +46,32 @@ public class NonceCheckServiceImpl implements CheckService {
             //        判断当前时间和timestamp的关系。
             if (System.currentTimeMillis() - Long.parseLong(requestHeader.getTimestamp()) > Long.parseLong(signNaslConfiguration.getSignMaxTime())) {
                 log.error("checkSign error，时间超出范围");
-                return true;
+                return false;
             }
         }
 //        校验timestamp、nonce和sign的关系。新增body
         if (!checkSign(requestHeader)) {
             log.warn("checkSign error，签名校验失败");
-            return true;
+            return false;
         }
         if (storageNaslConfiguration.getStorageStrategy() == null) {
             log.error("storageStrategy error，配置信息异常");
-            return true;
+            return false;
         }
         StorageService storageService = storageServiceMap.get(storageNaslConfiguration.getStorageStrategy());
         if (storageService == null) {
             log.error("storageStrategy error，配置信息异常");
-            return true;
+            return false;
         }
         Long timestamp;
         try {
             timestamp = Long.parseLong(signNaslConfiguration.getSignMaxTime());
         } catch (NumberFormatException e) {
             log.error("checkSign error，配置信息-时间格式异常", e);
-            return true;
+            return false;
         }
 //        查看60s内是否存在
-        storageService.checkAndAddIfAbsent(requestHeader.getNonce() + requestHeader.getTimestamp(), timestamp);
-        return true;
+        return storageService.checkAndAddIfAbsent(requestHeader.getNonce() + requestHeader.getTimestamp(), timestamp);
     }
 
 
