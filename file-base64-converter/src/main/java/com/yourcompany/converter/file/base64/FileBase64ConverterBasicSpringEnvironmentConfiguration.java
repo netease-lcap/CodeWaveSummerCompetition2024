5129498dc758db1de768fdb1c1fae8a097e6f599
芝梦 <145199960+zhimeng16@users.noreply.github.com>
2026-01-09 18:42:51 +0800

    Update FileBase64ConverterBasicSpringEnvironmentConfiguration.java

--- file-base64-converter/src/main/java/com/yourcompany/converter/file/base64/FileBase64ConverterBasicSpringEnvironmentConfiguration.java
+++ file-base64-converter/src/main/java/com/yourcompany/converter/file/base64/FileBase64ConverterBasicSpringEnvironmentConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 加入spring环境配置（在spring.factories中指定）
+ * 加入spring环境配置（在spring.factories中指定）...
  */
 @Configuration
 @ComponentScan(basePackageClasses = LibraryAutoScan.class)
